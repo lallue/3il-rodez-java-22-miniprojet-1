09de1807a7fb48080d8d5e09dfd34556a1d25892
lallue <alluel@3il.fr>
2023-12-14 11:18:01 +0100

    reglement de problème de reso et de aleatoire

--- src/fr/ecole3il/rodez2023/perlin/math/Bruit2D.java
+++ src/fr/ecole3il/rodez2023/perlin/math/Bruit2D.java
@@ -1,7 +1,7 @@
 package fr.ecole3il.rodez2023.perlin.math;
 
 /**
- * La classe abstraite Noise2D est une base pour la génération de bruit en 2D.
+ * La classe abstraite Bruit2D est une base pour la génération de bruit en 2D.
  * Elle définit des méthodes permettant d'obtenir des valeurs de bruit en deux dimensions.
  */
 public abstract class Bruit2D {
@@ -9,12 +9,17 @@ public abstract class Bruit2D {
     /** La graine utilisée pour la génération du bruit */
     private final long graine;
     
+    /** La résolution utilisée pour la génération du bruit */
+    private double resolution;
+    
     /**
-     * Constructeur de la classe Noise2D.
-     * @param seed2 La graine utilisée pour initialiser le générateur de bruit.
+     * Constructeur de la classe Bruit2D.
+     * @param graine La graine utilisée pour initialiser le générateur de bruit.
+     * @param resolution La résolution utilisée pour la génération du bruit.
      */
     public Bruit2D(long graine, double resolution) {
         this.graine = graine;
+        this.resolution = resolution;
     }
     
     /**
@@ -33,4 +38,19 @@ public abstract class Bruit2D {
         return graine;
     }
 
+    /**
+     * Méthode permettant d'obtenir la résolution utilisée pour la génération du bruit.
+     * @return La résolution utilisée pour la génération du bruit.
+     */
+    public double getResolution() {
+        return resolution;
+    }
+
+    /**
+     * Méthode permettant de définir la résolution utilisée pour la génération du bruit.
+     * @param resolution La nouvelle résolution à définir.
+     */
+    public void setResolution(double resolution) {
+        this.resolution = resolution;
+    }
 }
--- src/fr/ecole3il/rodez2023/perlin/math/BruitAleatoire.java
+++ src/fr/ecole3il/rodez2023/perlin/math/BruitAleatoire.java
@@ -3,11 +3,32 @@ package fr.ecole3il.rodez2023.perlin.math;
 import java.util.Random;
 
 /**
- * La classe RandomNoise étend la classe Noise2D et génère du bruit aléatoire en deux dimensions.
+ * La classe BruitAleatoire étend la classe Bruit2D et génère du bruit aléatoire en deux dimensions.
  * Elle utilise la classe Random de Java pour générer des valeurs aléatoires.
  */
 public class BruitAleatoire extends Bruit2D {
 
-    public BruitAleatoire(long graine, double resolution);
-    
+    /**
+     * Constructeur de la classe BruitAleatoire.
+     * @param graine La graine utilisée pour initialiser le générateur de bruit.
+     * @param resolution La résolution utilisée pour la génération du bruit.
+     */
+    public BruitAleatoire(long graine, double resolution) {
+        // Appel du constructeur de la classe mère avec la graine et la résolution
+        super(graine, resolution);
+    }
+
+    /**
+     * Méthode qui génère du bruit aléatoire en deux dimensions.
+     * Cette méthode ne dépend pas des coordonnées x et y.
+     * @param x Coordonnée x (non utilisée)
+     * @param y Coordonnée y (non utilisée)
+     * @return Valeur de bruit aléatoire entre 0.0 (inclus) et 1.0 (exclus).
+     */
+    @Override
+    public double bruit2D(double x, double y) {
+        // Utilisation de la classe Random pour générer du bruit aléatoire
+        Random random = new Random(getGraine());
+        return random.nextDouble();
+    }
 }
