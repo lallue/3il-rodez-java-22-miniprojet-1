e79939a5a5ace69fbce53716e220fdd6c150f1ef
lallue <alluel@3il.fr>
2023-12-15 11:32:25 +0100

    classe Terrain

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/Terrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/Terrain.java
@@ -1,5 +1,17 @@
 package fr.ecole3il.rodez2023.perlin.terrain.elements;
 
 public class Terrain {
-
+	private double hydrometrie;
+    private double temperature;
+    private double altitude;
+    
+	public double getHydrometrie() {
+		return hydrometrie;
+	}
+	public double getTemperature() {
+		return temperature;
+	}
+	public double getAltitude() {
+		return altitude;
+	}
 }
