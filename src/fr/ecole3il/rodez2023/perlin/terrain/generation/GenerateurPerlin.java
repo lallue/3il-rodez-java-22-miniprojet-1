403f7b6efb917d8075a08f27b2068fdfddb8dea0
lallue <alluel@3il.fr>
2024-02-14 21:00:24 +0100

    c

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
@@ -3,25 +3,30 @@ package fr.ecole3il.rodez2023.perlin.terrain.generation;
 import fr.ecole3il.rodez2023.perlin.math.BruitPerlin2D;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
 
-public abstract class GenerateurPerlin extends GenerateurCarte {
+public class GenerateurPerlin extends GenerateurCarte{
 
-    public GenerateurPerlin(long graine) {
-        super(graine);
-    }
+	public GenerateurPerlin(long graine) {
+		super(graine);
+		// TODO Auto-generated constructor stub
+	}
 
-    @Override
-    protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
-        double x = (double) i / largeur;
-        double y = (double) j / hauteur;
+	@Override
+	protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
+		
+		BruitPerlin2D altitudePerlin = new BruitPerlin2D(getGraine(), 1); 
+        BruitPerlin2D hydrometriePerlin = new BruitPerlin2D(getGraine(), 1); 
+        BruitPerlin2D temperaturePerlin = new BruitPerlin2D(getGraine() * 2, 1);
+        
+        double x = (double) i / (double) largeur;
+        double y = (double) j / (double) hauteur;
 
-        // Utilisation de la classe BruitPerlin2D pour générer le bruit de Perlin
-        BruitPerlin2D perlin2D = new BruitPerlin2D(getGraine(), 1.0);  // 1.0 est la résolution, vous pouvez ajuster cela
-        double bruit = perlin2D.bruit2D(x, y);
-
-        double altitude = bruit;
-        double hydrometrie = bruit * 2;  // La graine de l'hydrométrie est le double de la graine principale
-        double temperature = bruit * 4;  // La graine de la température est le double de l'hydrométrie
-
+        double altitude = altitudePerlin.bruit2D(x, y);
+        double hydrometrie = hydrometriePerlin.bruit2D(x, y);
+        double temperature = temperaturePerlin.bruit2D(x, y);
+        hydrometrie = (hydrometrie + 1) / 2; 
+        temperature = (temperature + 1) / 2 ; 
+        /**@return --> création d'un nouveau terrain*/
         return new Terrain(altitude, hydrometrie, temperature);
-    }
+	}
+
 }
