c682259f2d17e15ec9ec6238c4f2149dc9314773
lallue <alluel@3il.fr>
2024-02-14 20:24:27 +0100

    a

--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/AltitudeAffichee.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/AltitudeAffichee.java
@@ -1,5 +1,20 @@
 package fr.ecole3il.rodez2023.perlin.terrain.visualisation;
 
-public class AltitudeAffichee {
+public enum AltitudeAffichee {
+	BASSE(0.33),
+    MOYENNE(0.66),
+    ELEVEE(1.0),
+    FOND_MARIN(-1.0);
 
+
+    private final double valeurLimite;
+
+    AltitudeAffichee(double valeurLimite) {
+        this.valeurLimite = valeurLimite;
+    }
+
+    public double getValeurLimite() {
+        return valeurLimite;
+        
+    }
 }
