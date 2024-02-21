1a0751e3ddb15ba5ff020bfc6fb98cced575e87f
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-21 19:17:36 +0100

    javadoc et commentaire

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
@@ -5,25 +5,46 @@ import java.util.Random;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.MauvaiseValeurException;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
 
-
+/**
+ * La classe GenerateurAleatoire étend la classe abstraite GenerateurCarte
+ * et implémente la génération aléatoire d'une carte en utilisant une graine.
+ */
 public class GenerateurAleatoire extends GenerateurCarte {
 
-    Random random;
+    // Générateur de nombres aléatoires
+    private Random random;
 
+    /**
+     * Constructeur de la classe GenerateurAleatoire.
+     *
+     * @param graine La graine pour initialiser le générateur de nombres aléatoires.
+     */
     public GenerateurAleatoire(long graine) {
         super(graine);
-        this.random = new Random();
+        this.random = new Random(graine);
     }
 
+    /**
+     * Génère un terrain aléatoire en fonction des coordonnées spécifiées, de la largeur et de la hauteur.
+     *
+     * @param i        Coordonnée x de la position dans la carte.
+     * @param j        Coordonnée y de la position dans la carte.
+     * @param largeur  Largeur de la carte.
+     * @param hauteur  Hauteur de la carte.
+     * @return Un objet Terrain généré aléatoirement.
+     */
     @Override
     protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
+        // Génération de valeurs aléatoires pour l'altitude, l'hydrométrie et la température
         double altitude = this.random.nextDouble();
         double hydrometrie = this.random.nextDouble();
         double temperature = this.random.nextDouble();
 
         try {
+            // Création d'un objet Terrain avec les valeurs générées
             return new Terrain(altitude, hydrometrie, temperature);
         } catch (MauvaiseValeurException e) {
+            // En cas d'erreur, afficher la trace de la pile et retourner null
             e.printStackTrace();
             return null;
         }
