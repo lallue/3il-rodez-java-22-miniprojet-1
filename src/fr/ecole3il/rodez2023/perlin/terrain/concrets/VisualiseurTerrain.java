67147b4b5042d45aecd9f3f1b4f81bd4a9feb78c
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-21 19:27:51 +0100

    javadoc et commentaire

--- src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrain.java
@@ -9,15 +9,33 @@ import fr.ecole3il.rodez2023.perlin.terrain.visualisation.DetermineurTerrain;
 import fr.ecole3il.rodez2023.perlin.terrain.visualisation.HydrometrieAffichee;
 import fr.ecole3il.rodez2023.perlin.terrain.visualisation.TemperatureAffichee;
 
+/**
+ * La classe VisualiseurTerrain permet de visualiser les propriétés du terrain
+ * telles que l'altitude, l'hydrométrie, la température, le type de terrain, etc.
+ */
 public class VisualiseurTerrain {
     private DetermineurTerrain determineurTerrain;
     private Carte carte;
 
+    /**
+     * Constructeur de la classe VisualiseurTerrain.
+     *
+     * @param determineurTerrain L'objet qui détermine le type de terrain.
+     * @param carte              La carte à visualiser.
+     */
     public VisualiseurTerrain(DetermineurTerrain determineurTerrain, Carte carte) {
         this.determineurTerrain = determineurTerrain;
         this.carte = carte;
     }
 
+    /**
+     * Obtient l'altitude affichée pour la position spécifiée.
+     *
+     * @param x La coordonnée x dans la carte.
+     * @param y La coordonnée y dans la carte.
+     * @return L'altitude affichée.
+     * @throws TerrainInexistant Si le terrain demandé n'existe pas dans la carte.
+     */
     public AltitudeAffichee getAltitudeAffichee(int x, int y) throws TerrainInexistant {
         Terrain terrain = carte.getTerrain(x, y);
         double altitude = terrain.getAltitude();
@@ -33,6 +51,14 @@ public class VisualiseurTerrain {
         }
     }
 
+    /**
+     * Obtient l'hydrométrie affichée pour la position spécifiée.
+     *
+     * @param x La coordonnée x dans la carte.
+     * @param y La coordonnée y dans la carte.
+     * @return L'hydrométrie affichée.
+     * @throws TerrainInexistant Si le terrain demandé n'existe pas dans la carte.
+     */
     public HydrometrieAffichee getHydrometrieAffichee(int x, int y) throws TerrainInexistant {
         Terrain terrain = carte.getTerrain(x, y);
         double hydrometrie = terrain.getHydrometrie();
@@ -46,6 +72,14 @@ public class VisualiseurTerrain {
         }
     }
 
+    /**
+     * Obtient la température affichée pour la position spécifiée.
+     *
+     * @param x La coordonnée x dans la carte.
+     * @param y La coordonnée y dans la carte.
+     * @return La température affichée.
+     * @throws TerrainInexistant Si le terrain demandé n'existe pas dans la carte.
+     */
     public TemperatureAffichee getTemperatureAffichee(int x, int y) throws TerrainInexistant {
         Terrain terrain = carte.getTerrain(x, y);
         double temperature = terrain.getTemperature();
@@ -59,16 +93,34 @@ public class VisualiseurTerrain {
         }
     }
 
+    /**
+     * Obtient le type de terrain pour la position spécifiée en utilisant le détermineur de terrain.
+     *
+     * @param x La coordonnée x dans la carte.
+     * @param y La coordonnée y dans la carte.
+     * @return Le type de terrain.
+     * @throws TerrainInexistant Si le terrain demandé n'existe pas dans la carte.
+     */
     public TypeTerrain getTypeTerrain(int x, int y) throws TerrainInexistant {
         Terrain terrain = carte.getTerrain(x, y);
         TypeTerrain typeTerrain = determineurTerrain.determinerTerrain(terrain.getAltitude(), terrain.getHydrometrie(), terrain.getTemperature());
         return typeTerrain;
     }
 
+    /**
+     * Obtient le détermineur de terrain utilisé par le visualiseur.
+     *
+     * @return Le détermineur de terrain.
+     */
     public DetermineurTerrain getDetermineurTerrain() {
         return determineurTerrain;
     }
-    
+
+    /**
+     * Obtient la carte utilisée par le visualiseur.
+     *
+     * @return La carte.
+     */
     public Carte getCarte() {
         return carte;
     }
