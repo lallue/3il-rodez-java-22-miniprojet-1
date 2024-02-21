684b5d3b4d306383e7302f1d18452cfad97e5d2c
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-21 19:16:02 +0100

    javadoc et commentaire

--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/DetermineurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/DetermineurTerrain.java
@@ -2,9 +2,19 @@ package fr.ecole3il.rodez2023.perlin.terrain.visualisation;
 
 import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
 
+/**
+ * L'interface DetermineurTerrain définit la méthode permettant de déterminer le type de terrain
+ * en fonction de l'altitude, de l'hydrométrie et de la température.
+ */
 public interface DetermineurTerrain {
 
-	public TypeTerrain determinerTerrain(double altitude, double hydrometrie, double temperature);
-
-
+    /**
+     * Méthode permettant de déterminer le type de terrain en fonction de certains paramètres.
+     *
+     * @param altitude     L'altitude du terrain.
+     * @param hydrometrie   L'hydrométrie du terrain.
+     * @param temperature   La température du terrain.
+     * @return Le type de terrain déterminé.
+     */
+    TypeTerrain determinerTerrain(double altitude, double hydrometrie, double temperature);
 }
