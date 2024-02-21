1fff3fbf326ea8674e3d3400a12e978a786f2588
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-21 19:21:30 +0100

    javadoc et commentaire

--- src/fr/ecole3il/rodez2023/perlin/terrain/carte/TerrainInexistant.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/carte/TerrainInexistant.java
@@ -1,8 +1,16 @@
 package fr.ecole3il.rodez2023.perlin.terrain.carte;
 
+/**
+ * Exception levée lorsqu'un terrain demandé n'existe pas dans la carte.
+ */
 public class TerrainInexistant extends Exception {
 
-	public TerrainInexistant(String message) {
-		super(message);
-	}
+    /**
+     * Constructeur de la classe TerrainInexistant.
+     *
+     * @param message Le message décrivant l'exception.
+     */
+    public TerrainInexistant(String message) {
+        super(message);
+    }
 }
