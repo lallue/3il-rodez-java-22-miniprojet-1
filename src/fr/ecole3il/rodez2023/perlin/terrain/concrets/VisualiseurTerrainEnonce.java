7929cba7376b167108d45e84d8f0f03a0b742afa
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-21 19:25:43 +0100

    javadoc et commentaire

--- src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrainEnonce.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrainEnonce.java
@@ -2,8 +2,18 @@ package fr.ecole3il.rodez2023.perlin.terrain.concrets;
 
 import fr.ecole3il.rodez2023.perlin.terrain.carte.Carte;
 
+/**
+ * La classe VisualiseurTerrainEnonce est une implémentation concrète de la classe abstraite VisualiseurTerrain.
+ * Elle utilise le DetermineurTerrainEnonce pour déterminer le type de terrain à afficher sur la carte.
+ */
 public class VisualiseurTerrainEnonce extends VisualiseurTerrain {
-	public VisualiseurTerrainEnonce(Carte c) {
-		super(new DetermineurTerrainEnonce(), c);
-	}
+
+    /**
+     * Constructeur de la classe VisualiseurTerrainEnonce.
+     *
+     * @param c La carte à visualiser.
+     */
+    public VisualiseurTerrainEnonce(Carte c) {
+        super(new DetermineurTerrainEnonce(), c);
+    }
 }
