a99fa916adc71e6325ac746dbbaba5a515f7eeee
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-22 18:51:03 +0100

    ajout javadoc + commentaire

--- src/fr/ecole3il/rodez2023/perlin/test/UtilsTest.java
+++ src/fr/ecole3il/rodez2023/perlin/test/UtilsTest.java
@@ -6,19 +6,33 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Classe de test pour la classe Utils.
+ */
 class UtilsTest {
 
+    /**
+     * Classe imbriquée pour tester la méthode statique min de la classe Utils.
+     */
     @Nested
-    class MinTest{
+    class MinTest {
 
+        /**
+         * Teste la méthode min avec la largeur en tant que valeur minimale.
+         * Vérifie que la méthode renvoie la valeur minimale correcte.
+         */
         @Test
-        void min_widthIsMin(){
-            assertEquals(1, Utils.min(1,3));
+        void min_widthIsMin() {
+            assertEquals(1, Utils.min(1, 3));
         }
 
+        /**
+         * Teste la méthode min avec la hauteur en tant que valeur minimale.
+         * Vérifie que la méthode renvoie la valeur minimale correcte.
+         */
         @Test
-        void min_heaightIsMin(){
-            assertEquals(1, Utils.min(3,1));
+        void min_heightIsMin() {
+            assertEquals(1, Utils.min(3, 1));
         }
     }
 }
