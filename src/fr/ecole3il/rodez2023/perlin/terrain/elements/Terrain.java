87417b756ce58c786ac7833387482b8000e9b7df
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-22 09:10:26 +0100

    Réglage rpoblème hydro et temp

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/Terrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/Terrain.java
@@ -74,6 +74,7 @@ public class Terrain {
      * @throws MauvaiseValeurException Si la valeur est en dehors de la plage autorisée [0, 1].
      */
     public void setTemperature(double temperature) {
+    	temperature = Math.abs(temperature);
         if (temperature < 0 || temperature > 1) {
             throw new MauvaiseValeurException("La température doit être comprise entre 0 et 1.");
         }
